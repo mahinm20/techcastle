d0ffbae657998a7db4c587280d0880fb6aeaf58f
Mahin <mahinmalhotra20@gmail.com>
2020-08-15 00:18:13 +0530

    Added cart and checkout pages with rendering data

--- store/models.py
+++ store/models.py
@@ -64,7 +64,7 @@ class Accessories(models.Model):
         return url      
 
 class Order(models.Model):
-    customer = models.ForeignKey(Customer,on_delete=models.SET_NULL,blank=True,null=True)
+    customer = models.ForeignKey(Customer,on_delete=models.CASCADE,blank=True,null=True)
     date_ordered = models.DateTimeField(auto_now_add=True)
     completed = models.BooleanField(default=False,null=True,blank=False)
     transaction_id = models.CharField(max_length=200)
@@ -74,15 +74,17 @@ class Order(models.Model):
 
     @property
     def get_cart_total(self):
-        orderitems  = self.orderitem_set.all()
-        total = sum([item.get_laptop_total for item in orderitems])
+        orderitems  = OrderItem.objects.all()
+        total = sum([item.get_laptop_total for item in orderitems]) + sum([item.get_acc_total for item in orderitems]) 
+        + sum([item.get_mobile_total for item in orderitems])
+
         return total      
     
     @property
     def get_cart_item(self):
-        orderitems = self.orderitem_set.all()
-        total = sum([item.quantity for item in orderitems])
-        return total 
+        orderitems = OrderItem.objects.all()
+        total = sum([i.quantity for i in orderitems]) 
+        return total
     
     
 
@@ -90,37 +92,44 @@ class Order(models.Model):
 
 
 class OrderItem(models.Model):
-    product_m = models.ForeignKey(Mobile,on_delete=models.SET_NULL,null=True,blank=True)
-    product_l = models.ForeignKey(Laptop,on_delete=models.SET_NULL,null=True,blank=True)
-    product_a = models.ForeignKey(Accessories,on_delete=models.SET_NULL,null=True,blank=True)
+    product_m = models.ForeignKey(Mobile,on_delete=models.CASCADE,null=True,blank=True)
+    product_l = models.ForeignKey(Laptop,on_delete=models.CASCADE,null=True,blank=True)
+    product_a = models.ForeignKey(Accessories,on_delete=models.CASCADE,null=True,blank=True)
     order = models.ForeignKey(Order,on_delete=models.SET_NULL,null=True,blank=True)
-    quantity = models.IntegerField(default=0,null=True,blank=True)
-
+    
+    quantity = models.IntegerField(default=0,null=True,blank=True)    
     date_added = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        if (self.product_l is not None):
-            return (self.product_l.name)
-        elif (self.product_a is not None):
-            return (self.product_a.name)
-        elif (self.product_m is not None):
-            return (self.product_m.name)    
-
+    # def __str__(self):
+    #     if (self.product_l__name is not None):
+    #         return str(self.product_l__name)
+    #     elif (self.product_a__name is not None):
+    #         return str(self.product_a__name)
+    #     elif (self.product_m__name is not None):
+    #         return str(self.product_m__name)    
     @property
-    def get_mobile_total(self):
-        total_m =  self.product_m.price * self.quantity
-        return total_m    
-
-    @property
-    def get_acc_total(self):
-        total_a =  self.product_a.price * self.quantity
-        return total_a   
+    def get_laptop_total(self):  
+        if self.product_l:  
+            total_l =  self.product_l.price * self.quantity  
+            return total_l
+        else:
+            return 0
     
     @property
-    def get_laptop_total(self):
-        total_l =  self.product_l.price * self.quantity
-        return total_l                      
-    
+    def get_acc_total(self):  
+        if self.product_a:  
+            total_a =  self.product_a.price * self.quantity  
+            return total_a
+        else:
+            return 0                      
+    @property
+    def get_mobile_total(self):  
+        if self.product_m:  
+            total_m =  self.product_m.price * self.quantity  
+            return total_m
+        else:
+            return 0
+
    
     
 
